48c15f0c645ce283bc2f542ddc46b2b575d12bff
Carl Tuttle <cgtuttle@gmail.com>
2015-12-12 11:31:18 -0600

    Debugging mail issues

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -3,6 +3,9 @@ class InvitationMailer < ActionMailer::Base
 
 	def invite_message(user, from, subject, content, sender)
 		@user = user
+		user.invite! do |u| # This is necessary to generate a user token. It will not generate a new user record.
+			u.skip_invitation = true
+		end
 		@token = user.raw_invitation_token
 		puts "raw_invitation_token = #{user.raw_invitation_token}"
 		invitation_link = accept_user_invitation_url(:invitation_token => @token)
